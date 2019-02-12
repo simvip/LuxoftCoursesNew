38b9174da4a6b6e19587eec94eb72b659cade417
ivan.sliusar <ivan.sliusar.ua@gmail.com>
2019-02-12 17:05:28 +0200

    Fix Selection sort

--- algorithms/src/test/java/SelectionSortTest.java
+++ algorithms/src/test/java/SelectionSortTest.java
@@ -8,14 +8,14 @@ public class SelectionSortTest {
     @Test
     public void sort() {
         SelectionSort selectionSort = new SelectionSort();
-        int[] testArray = {1, 4, 5, 4};
+        int[] testArray = {1, 5, 4, 4};
         int[] resultArray = selectionSort.sort(testArray);
-        int[] expectArray = {5, 4, 4, 1};
+        int[] expectArray = {1, 4, 4, 5};
         assertThat(resultArray, is(expectArray));
 
         int[] testArray2 = {1, 4, -5, 4};
         int[] resultArray2 = selectionSort.sort(testArray2);
-        int[] expectArray2 = {4, 4, 1, -5};
+        int[] expectArray2 = {-5, 1, 4, 4};
         assertThat(resultArray2, is(expectArray2));
     }
 }
